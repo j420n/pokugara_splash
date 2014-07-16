809ed02a2c194163b0245ab4fd5d5e9d2c7bb6de
Noel Sharpe <noels@radnetwork.co.uk>
2014-07-16 17:58:07 +0100

    Change map colours and zoom out a bit.

--- js/pokugara.js
+++ js/pokugara.js
@@ -20,118 +20,60 @@ $(function() {
 
 //Google Map Skin - Get more at http://snazzymaps.com/
 var myOptions = {
-    zoom: 15,
+    zoom: 13,
     center: new google.maps.LatLng(-17.787348, 31.083841),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     disableDefaultUI: true,
-    styles: [{
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 17
-        }]
-    }, {
-        "featureType": "landscape",
-        "elementType": "geometry",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 20
-        }]
-    }, {
-        "featureType": "road.highway",
-        "elementType": "geometry.fill",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 17
-        }]
-    }, {
-        "featureType": "road.highway",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 29
-        }, {
-            "weight": 0.2
-        }]
-    }, {
-        "featureType": "road.arterial",
-        "elementType": "geometry",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 18
-        }]
-    }, {
-        "featureType": "road.local",
-        "elementType": "geometry",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 16
-        }]
-    }, {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 21
-        }]
-    }, {
-        "elementType": "labels.text.stroke",
-        "stylers": [{
-            "visibility": "on"
-        }, {
-            "color": "#000000"
-        }, {
-            "lightness": 16
-        }]
-    }, {
-        "elementType": "labels.text.fill",
-        "stylers": [{
-            "saturation": 36
-        }, {
-            "color": "#000000"
-        }, {
-            "lightness": 40
-        }]
-    }, {
-        "elementType": "labels.icon",
-        "stylers": [{
-            "visibility": "off"
-        }]
-    }, {
-        "featureType": "transit",
-        "elementType": "geometry",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 19
-        }]
-    }, {
-        "featureType": "administrative",
-        "elementType": "geometry.fill",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 20
-        }]
-    }, {
-        "featureType": "administrative",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 17
-        }, {
-            "weight": 1.2
-        }]
-    }]
+    styles: [
+        {"elementType": "geometry", "stylers": [
+            {"hue": "#ff4400"},
+            {"saturation": -68},
+            {"lightness": -4},
+            {"gamma": 0.72}
+        ]},
+        {"featureType": "road", "elementType": "labels.icon"},
+        {"featureType": "landscape.man_made", "elementType": "geometry", "stylers": [
+            {"hue": "#0077ff"},
+            {"gamma": 3.1}
+        ]},
+        {"featureType": "water", "stylers": [
+            {"hue": "#00ccff"},
+            {"gamma": 0.44},
+            {"saturation": -33}
+        ]},
+        {"featureType": "poi.park", "stylers": [
+            {"hue": "#44ff00"},
+            {"saturation": -23}
+        ]},
+        {"featureType": "water", "elementType": "labels.text.fill", "stylers": [
+            {"hue": "#007fff"},
+            {"gamma": 0.77},
+            {"saturation": 65},
+            {"lightness": 99}
+        ]},
+        {"featureType": "water", "elementType": "labels.text.stroke", "stylers": [
+            {"gamma": 0.11},
+            {"weight": 5.6},
+            {"saturation": 99},
+            {"hue": "#0091ff"},
+            {"lightness": -86}
+        ]},
+        {"featureType": "transit.line", "elementType": "geometry", "stylers": [
+            {"lightness": -48},
+            {"hue": "#ff5e00"},
+            {"gamma": 1.2},
+            {"saturation": -23}
+        ]},
+        {"featureType": "transit", "elementType": "labels.text.stroke", "stylers": [
+            {"saturation": -64},
+            {"hue": "#ff9100"},
+            {"lightness": 16},
+            {"gamma": 0.47},
+            {"weight": 2.7}
+        ]}
+    ]
 };
 
 var map = new google.maps.Map(document.getElementById('map'), myOptions);
+
+
