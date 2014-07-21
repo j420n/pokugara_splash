f128ed2b72568180c649c0135289e1a65907a83e
Noel Sharpe <noels@radnetwork.co.uk>
2014-07-21 09:40:50 +0100

    Add content type to make XdomainRequest happy.

--- js/register.js
+++ js/register.js
@@ -29,6 +29,7 @@ $(document).ready(function () {
         request = $.ajax({
             url: "https://script.google.com/macros/s/AKfycbxBodnzSduWj3ah55EFIziHgJitPvOmBNfbfb45fpe_Pjcmf_5f/exec",
             type: "post",
+            contentType: 'text/plain',
             crossDomain: true,
             data: serializedData
         });
